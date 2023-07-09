c8c643aff570fbbe9ec5e06bcd7ee1a1aea83d47
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2023-07-09 08:47:33 +0200

    Add socialnetwork pictures in footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { styled } from 'styled-components';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import FacebookPicture from '../../assets/img/facebook.png';
+import SnapPicture from '../../assets/img/snapchat.png';
+import TwitterPicture from '../../assets/img/twitter.png'
 
 
 
@@ -12,7 +16,9 @@ function Footer() {
        La Politiqe de confidentialité ici
       </div>
       <div>
-       Les réseaux sociaux ici
+      <Link to="/user"> <img src= {FacebookPicture} alt='' className='facebook'/> </Link>
+      <Link to="/user"> <img src= {SnapPicture} alt='' className='snap'/> </Link>
+      <Link to="/user"> <img src= {TwitterPicture} alt='' className='twitter'/> </Link>
       </div>
       <div>
        Le CopyWright ici
@@ -27,6 +33,11 @@ display: flex;
 justify-content: space-around;
 align-items: center;
 border-top: 1px solid black;
+
+& img{
+  width : 5%;
+
+};
 
 
 `;
